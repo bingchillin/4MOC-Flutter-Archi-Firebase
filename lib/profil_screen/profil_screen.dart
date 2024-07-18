fe5e1ddc50b802e7cd1771f0169a26918ebb7909
Kenny <92927093+Pokoki@users.noreply.github.com>
2024-07-18 02:47:26 +0200

    Update profil_screen.dart

--- lib/profil_screen/profil_screen.dart
+++ lib/profil_screen/profil_screen.dart
@@ -53,41 +53,24 @@ class UserProfilWidget extends StatelessWidget {
               children: [
                 ListTile(
                   leading: const CircleAvatar(child: Icon(Icons.account_circle_rounded)),
-                  title: isEditing
-                      ? TextField(controller: TextEditingController(text: 'Kenny'))
-                      : const Text('Nom'),
+                  title:  const Text('Nom'),
                   subtitle: isEditing
                       ? TextField(controller: TextEditingController(text: 'Kenny'))
                       : const Text('Kenny'),
                 ),
-                ListTile(
-                  leading: const CircleAvatar(radius: 0),
-                  subtitle: isEditing
-                      ? TextField(controller: TextEditingController(text: "Description Bien longue zeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebi"))
-                      : const Text("Description Bien longue zeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebi"),
+                const ListTile(
+                  leading: CircleAvatar(radius: 0),
+                  subtitle: Text("Description Bien longue zeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebi"),
                 ),
               ],
             ),
             const Divider(height: 0),
             ListTile(
               leading: const CircleAvatar(child: Icon(Icons.info)),
-              title: isEditing
-                  ? TextField(controller: TextEditingController(text: 'Infos'))
-                  : const Text('Infos'),
+              title: const Text('Infos'),
               subtitle: isEditing
                   ? TextField(controller: TextEditingController(text: 'Longer supporting text to demonstrate how the text wraps and how the leading and trailing widgets are centered vertically with the text.'))
                   : const Text('Longer supporting text to demonstrate how the text wraps and how the leading and trailing widgets are centered vertically with the text.'),
-            ),
-            const Divider(height: 0),
-            ListTile(
-              leading: const CircleAvatar(child: Icon(Icons.email)),
-              title: isEditing
-                  ? TextField(controller: TextEditingController(text: 'mail'))
-                  : const Text('mail'),
-              subtitle: isEditing
-                  ? TextField(controller: TextEditingController(text: "a@a.com"))
-                  : const Text("a@a.com"),
-              isThreeLine: true,
             ),
           ],
         );
