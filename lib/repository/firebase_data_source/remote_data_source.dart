c15537a1d380e693616f701d46b0f30b30f6f3ee
bingchillin <92374981+bingchillin@users.noreply.github.com>
2024-07-19 22:47:03 +0200

    Added functions to remote data source

--- lib/repository/firebase_data_source/remote_data_source.dart
+++ lib/repository/firebase_data_source/remote_data_source.dart
@@ -4,4 +4,6 @@ import '../../models/user.dart';
 abstract class RemoteDataSource {
   Future<AppUser> getUserProfil(String mail);
   Future<void> updateUserProfil(AppUser user);
+  Future<String?> getCurrentUserEmail();
+  Future<List<AppUser>> getAllUsers();
 }
