4fec9c2e2be379f71a102783f5065468da15752f
Kenny <92927093+Pokoki@users.noreply.github.com>
2024-07-19 23:11:00 +0200

    create AppRepository

---
+++ lib/repository/app_repository.dart
@@ -0,0 +1,28 @@
+import 'package:projet_flutter_firebase/models/user.dart';
+
+import 'firebase_data_source/remote_data_source.dart';
+
+class AppRepository{
+  final RemoteDataSource remoteDataSource;
+
+  AppRepository({
+    required this.remoteDataSource,
+  });
+
+  Future<AppUser> getUserProfil(String mail) async {
+    return remoteDataSource.getUserProfil(mail);
+  }
+
+  Future<void> updateUserProfil(AppUser user) async {
+    return remoteDataSource.updateUserProfil(user);
+  }
+
+  Future<String?> getCurrentUserEmail() async {
+    return remoteDataSource.getCurrentUserEmail();
+  }
+
+  Future<List<AppUser>> getAllUsers() async {
+    return remoteDataSource.getAllUsers();
+  }
+
+}
