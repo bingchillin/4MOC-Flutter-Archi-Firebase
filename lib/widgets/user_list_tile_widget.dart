80e496e2b6bf21a01dbb384cbb0585fb44de3f92
bingchillin <92374981+bingchillin@users.noreply.github.com>
2024-07-20 21:48:58 +0200

    Now removes icon on add

--- lib/widgets/user_list_tile_widget.dart
+++ lib/widgets/user_list_tile_widget.dart
@@ -1,30 +1,64 @@
 import 'package:flutter/material.dart';
 
-class UserListTileWidget extends StatelessWidget {
+class UserListTileWidget extends StatefulWidget {
   final String pseudo;
   final String email;
   final bool isFriend;
-  final VoidCallback? onAddPressed;
+  final Future<void> Function()? onAddPressed;
 
   const UserListTileWidget({
     super.key,
     required this.pseudo,
     required this.email,
-    this.isFriend = false,
+    required this.isFriend,
     this.onAddPressed,
   });
 
   @override
+  _UserListTileWidgetState createState() => _UserListTileWidgetState();
+}
+
+class _UserListTileWidgetState extends State<UserListTileWidget> {
+  bool _isLoading = false;
+  bool _isFriend = false;
+
+  _UserListTileWidgetState() : _isFriend = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFriend = widget.isFriend; // Initialiser _isFriend avec la valeur passée au widget
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(pseudo),
-      subtitle: Text(email),
-      trailing: isFriend
+      title: Text(widget.pseudo),
+      subtitle: Text(widget.email),
+      trailing: _isFriend
           ? null
+          : (_isLoading
+          ? const CircularProgressIndicator()
           : IconButton(
         icon: const Icon(Icons.person_add),
-        onPressed: onAddPressed,
-      ),
+        onPressed: () async {
+          if (widget.onAddPressed != null) {
+            setState(() => _isLoading = true);
+            try {
+              await widget.onAddPressed!();
+              setState(() {
+                _isFriend = true;
+                _isLoading = false;
+              });
+            } catch (e) {
+              setState(() => _isLoading = false);
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Erreur lors de l\'ajout du contact.'))
+              );
+            }
+          }
+        },
+      )),
     );
   }
 }
